8c06cf721f1eb889f37f411ed7670bc04acbc0df
João Henrique <161750294+jaohss@users.noreply.github.com>
2024-11-13 19:40:45 -0300

    Update AtualizacaoDados.java

--- CONEXAOBD/src/conexao_jaohss/AtualizacaoDados.java
+++ CONEXAOBD/src/conexao_jaohss/AtualizacaoDados.java
@@ -1,28 +1,29 @@
 package conexao_jaohss;
 
+//importando package SQL
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class AtualizacaoDados {
-    public void atualizarDados() {
+    public void atualizarDados() {//Método para atualizar os dados
         Connection conexao = ConexaoBD.conectar();
         if(conexao != null){
             String sql = "UPDATE clubes SET nome = ?, ano_fundacao = ? WHERE id = ?";
             Scanner scn = new Scanner(System.in);
             try{
                 System.out.println("Digite o ID do clube que deseja atualizar: ");
-                int id = scn.nextInt();
+                int id = scn.nextInt();//recebe qual id do clube que deseja atualizar
                 scn.nextLine();
 
                 System.out.println("Digite o novo nome do clube: ");
-                String nome = scn.nextLine();
+                String nome = scn.nextLine();//recebe o nome novo clube
 
                 System.out.println("Digite o novo ano de fundação do clube: ");
-                int ano_fundacao = scn.nextInt();
+                int ano_fundacao = scn.nextInt();//recebe o novo ano de função do clube
 
-                PreparedStatement stmt =  conexao.prepareStatement(sql);
+                PreparedStatement stmt =  conexao.prepareStatement(sql);//definindo a ordem das variaveis de acordo com a string sql
                 stmt.setString(1, nome);
                 stmt.setInt(2, ano_fundacao);
                 stmt.setInt(3, id);
@@ -36,7 +37,7 @@ public class AtualizacaoDados {
                     System.out.println("Nenhum registro encontrado com ID especificado");
                 }
 
-            }catch(SQLException e){
+            }catch(SQLException e){//Erro se houver falha ao atualizar os dados
                 System.err.println("Erro ao atualizar dados dados: " + e.getMessage());
             } finally{
                 try{
