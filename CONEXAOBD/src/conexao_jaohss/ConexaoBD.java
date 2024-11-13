14bc079f684e805ff979374c14b222fad39a7cfb
João Henrique <161750294+jaohss@users.noreply.github.com>
2024-11-13 19:45:02 -0300

    Update ConexaoBD.java

--- CONEXAOBD/src/conexao_jaohss/ConexaoBD.java
+++ CONEXAOBD/src/conexao_jaohss/ConexaoBD.java
@@ -5,12 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoBD{
-    private static final String URL = "jdbc:mysql://localhost:3306/CONEXAOBD_JAVA";
-    private static final String USUARIO = "root";
-    private static final String SENHA = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/CONEXAOBD_JAVA";//definindo o caminho/URL do banco de dados
+    private static final String USUARIO = "root"; //definindo o nome do usuário do banco de dados no wampserver
+    private static final String SENHA = ""; //definindo a senha do wampserver
 
+    //Método para fazer a conexão com o banco de dados
     public static Connection conectar() {
         try {
+            //Carregando o driver
             System.out.println("Tentando carregar o driver...");
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver carregado com sucesso !");
@@ -21,15 +23,16 @@ public class ConexaoBD{
             return conexao;
             
             
-        } catch (ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {//mensagem de erro caso o driver não seja encontrado
             System.err.println("Driver não encontrado: " + e.getMessage());
-        } catch (SQLException e) {
+        } catch (SQLException e) {//mensagem de erro caso haja falha ao conectar com banco de dados
             System.err.println("Erro ao conectar: " + e.getMessage());
         }
         return null;
     }
 
     public static void main(String[] args) {
+        //Execução da conexão com o banco de dados
         conectar();
     }
 }
