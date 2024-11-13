ac40da59270deda8024d1f5d7db4e7a07453ae44
João Henrique <161750294+jaohss@users.noreply.github.com>
2024-11-13 19:49:47 -0300

    Update ConsultaDados.java

--- CONEXAOBD/src/conexao_jaohss/ConsultaDados.java
+++ CONEXAOBD/src/conexao_jaohss/ConsultaDados.java
@@ -7,20 +7,23 @@ import java.sql.SQLException;
 
 
 public class ConsultaDados {
+    //Método para consulta de dados
     public void consultaDados() {
         Connection conexao = ConexaoBD.conectar();
         if(conexao != null){
-            String sql = "SELECT * FROM clubes";
+            String sql = "SELECT * FROM clubes"; //sintaxe seletora para consulta de todos os itens do banco de dados
             try{
                 PreparedStatement stmt = conexao.prepareStatement(sql);
                 ResultSet rs = stmt.executeQuery();
 
                 System.out.println("Registros da tabela 'clubes':");
                 while(rs.next()){
+                    //definição das variáveis que serão consultadas
                     int id = rs.getInt("id");
                     String nome = rs.getString("nome");
                     int ano_fundacao = rs.getInt("ano_fundacao");
-
+                    
+                    //Exibe a consulta do banco de dados
                     System.out.println("ID: "+ id + ", Nome: " + nome + ", Ano de fundacao: "+ano_fundacao);
                 }
             }catch (SQLException e){
