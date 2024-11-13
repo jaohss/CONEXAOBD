50424eed848cb38782ed3d40c2abb4295ea0562d
João Henrique <161750294+jaohss@users.noreply.github.com>
2024-11-13 19:36:52 -0300

    Update Main.java

--- CONEXAOBD/src/Main.java
+++ CONEXAOBD/src/Main.java
@@ -6,15 +6,17 @@ import conexao_jaohss.RemoverDados;
 
 public class Main {
     public static void main(String[] args)  {
+//Instanciando objetos
         Scanner scn = new Scanner(System.in);
         InserirDados id = new InserirDados();
         RemoverDados rd = new RemoverDados();
         ConsultaDados cd = new ConsultaDados();
         AtualizacaoDados ad = new AtualizacaoDados();
-        
+
+        //Definindo a variável de opção
         int opcao = -1;
 
-        do {
+        do {//Estrutura de repetição para executar o programa pelo menos uma vez
             System.out.println("=====MENU PRINCIPAL=====");
             System.out.println("1 - Inserir clube");
             System.out.println("2 - Atualizar clube");
@@ -22,28 +24,21 @@ public class Main {
             System.out.println("4 - Ler registros dos clubes ");
             System.out.println("0 - Sair");
             System.out.print("Escolha uma opção: ");
-
-            while (!scn.hasNextInt()) {
-                System.out.println("Entrada inválida! Por favor, digite um número.");
-                scn.next(); // descarta a entrada incorreta
-                System.out.print("Escolha uma opção: ");
-            }
-
             opcao = scn.nextInt();
             scn.nextLine(); // limpa o buffer
-            
+
             switch (opcao) {
                 case 1:
-                    id.inserirDados();
+                    id.inserirDados();//chama o método de inserção de dados
                     break;
                 case 2:
-                    ad.atualizarDados();
+                    ad.atualizarDados();//chama o método de atualização de dados
                     break;
                 case 3:
-                    rd.removerDados();
+                    rd.removerDados();//chama o método de remoção de dados
                     break;
                 case 4:
-                    cd.consultaDados();
+                    cd.consultaDados();//chama o método de consulta de dados
                     break;
                 case 0:
                     System.out.println("Saindo...");
